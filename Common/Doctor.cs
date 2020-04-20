0fb7be546d9b8903a67c491f52f5fca5c781a54d
Guy <guycohen08@gmail.com>
2020-04-20 10:43:27 +0300

    Added MongoDB configuration

--- Common/Doctor.cs
+++ Common/Doctor.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace Common
 {
     public class Doctor : IPerson
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
         public string ObjectId { get; set; }
+
+        [BsonElement("Name")]
         public string Name { get; set; }
+
+        [BsonElement("Birthday")]
         public DateTime Birthday { get; set; }
+
+        [BsonElement("Address")]
         public string Address { get; set; }
 
         public Doctor(string objectId, string name, DateTime birthday, string address)
---
+++ Server/Server/Models/MongoDBSettings.cs
@@ -0,0 +1,16 @@
+﻿namespace Server.Models
+{
+    public class MongoDBSettings : IMongoDBSettings
+    {
+        public string DoctorsCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+
+    public interface IMongoDBSettings
+    {
+        string DoctorsCollectionName { get; set; }
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+    }
+}
